02011ef531fe6b26a839e923855fe86f3b8a9ea9
Davidner <Davinderraju@gmail.com>
2025-10-01 13:51:58 -0700

    Created SpecUtil to handle common api request config and common assertions.

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -1,18 +1,15 @@
 package com.api.tests;
 
-import static org.hamcrest.Matchers. *;
 
 import java.io.IOException;
 
 import org.testng.annotations.Test;
 
-import static com.api.utils.AuthTokenProvider.*;
+
 
 import static com.api.constants.Role.*;
-import com.api.utils.ConfigManager;
 
-import io.restassured.http.ContentType;
-import io.restassured.http.Header;
+import com.api.utils.SpecUtil;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 import static io.restassured.RestAssured.*;
@@ -21,24 +18,13 @@ public class UserDetailsAPITest {
 	
 	@Test
 	public void userDetailsAPIRequest() throws IOException{
-		Header header = new Header("Authorization" , getToken(SUP));
 		
 		given()
-			.baseUri(ConfigManager.getProperty("BASE_URI"))
-		.and()
-			.header(header)
-		.and()
-			.accept(ContentType.JSON)
-			.log().uri()
-			.log().headers()
-			.log().method()
-			.log().body()
+			.spec(SpecUtil.requestSpecWithAuth(FD))
 		.when()
 			.get("userdetails")
 		.then()
-			.log().all()
-			.statusCode(200)
-			.time(lessThan(10000L))
+			.spec(SpecUtil.responsSpec_OK())
 		.and()
 			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/UserDetailsResponseSchema.json"));
 			
