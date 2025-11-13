eb3ccd012ca60e881dc1fcfd082fcbfcfa476689
Davidner <Davinderraju@gmail.com>
2025-11-12 22:22:01 -0800

    Completed the Requirement 11
    Created Excel Reader Utility that will read the excel file from the
    testData folder and will be used in the DataProvider Component.
    
    Test have been added in the testng-datadriven.xml

--- src/test/java/com/api/utils/ExcelReaderUtil.java
+++ src/test/java/com/api/utils/ExcelReaderUtil.java
@@ -2,41 +2,49 @@ package com.api.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.api.request.model.UserCredentials;
+import com.dataproviders.api.bean.UserBean;
+import com.poiji.bind.Poiji;
+
 public class ExcelReaderUtil {
 
-	public static void main(String[] args) throws IOException {
-		// Apache POI OOXML LIB
+	private ExcelReaderUtil() {
 		
-		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/PhoenixTestData.xlsx");
-		XSSFWorkbook myWorkbook = new XSSFWorkbook(input);
-		
-		//Focus on the sheet
-		
-		XSSFSheet mySheet = myWorkbook.getSheet("LoginTestData");
-		
-		XSSFRow myRow ;
-		XSSFCell myCell;
-		
-		int lastRowIndex = mySheet.getLastRowNum();
-		
-		XSSFRow rowHeader = mySheet.getRow(0);
-		int lastIndexOfCol = rowHeader.getLastCellNum()-1; // return total no of col so we sub 1
-		
-		for(int rowIndex= 0; rowIndex<=lastRowIndex ; rowIndex++) {
-			for(int colIndex=0; colIndex<= lastIndexOfCol ; colIndex++){
-				
-				myRow =mySheet.getRow(rowIndex);
-				 myCell= myRow.getCell(colIndex);
-				 System.out.print(myCell+ " ");
-			}
-			System.out.println("");
-		}
 	}
+	public static <T> Iterator<T> loadTestData(String pathOfExcelFile,String sheetName, Class<T> clazz)  {
+		// Apache POI OOXML LIB
+
+		InputStream input = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(pathOfExcelFile);
+		XSSFWorkbook myWorkbook=null;
+		try {
+			myWorkbook = new XSSFWorkbook(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Focus on the sheet
+
+		XSSFSheet mySheet = myWorkbook.getSheet(sheetName);
+
+		
+		List<T>dataList =Poiji.fromExcel(mySheet, clazz);
+		return dataList.iterator();
+		
+		
+	}
+	
+	
 
 }
