7fd4516b6245b64492924c64bf82cf5014781fda
Davidner <Davinderraju@gmail.com>
2025-11-26 09:09:23 -0800

    Created Customer Dao and Customer DB for requirement 13

---
+++ src/test/java/com/database/dao/DemoDaoRunner.java
@@ -0,0 +1,12 @@
+package com.database.dao;
+
+import java.sql.SQLException;
+
+import com.database.model.CustomerDBModel;
+
+public class DemoDaoRunner {
+	public static void main(String[] args) throws SQLException {
+		CustomerDBModel customerDBModel=  CustomerDao.getCustomerInfo();
+		System.out.println(customerDBModel);
+	}
+}
---
+++ src/test/java/com/database/model/DemoRunner.java
@@ -0,0 +1,10 @@
+package com.database.model;
+
+public class DemoRunner {
+	public static void main(String[] args) {
+			
+		CustomerDBModel customerDBModel = new CustomerDBModel("6","5","4","3","2","1");
+		
+		System.out.println(customerDBModel);
+	}
+}
