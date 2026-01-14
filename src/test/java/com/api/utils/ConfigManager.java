d471dd25b3787cef6d5432c5c5fbbf5d4b987643
Davidner <Davinderraju@gmail.com>
2026-01-14 14:15:46 -0800

    Added Allure Report to the framework
    created a AllureEnvironemntWriterUtil that show env info in the report
    
    Changed the ConfigManager env variable to public

--- src/test/java/com/api/utils/ConfigManager.java
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -13,7 +13,7 @@ public class ConfigManager {
 
 	private static Properties prop = new Properties();
 	private static String path = "Config/config.properties";
-	private static String env;
+	public static String env;
 	private static final Logger LOGGER = LogManager.getLogger(ConfigManager.class);
 
 	private ConfigManager() {
